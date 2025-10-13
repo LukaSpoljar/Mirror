3652553cc7fdd6779bf856572ff2cc07d44d8075
Ivan Luka Špoljar <lukispoljar@gmail.com>
2025-10-13 21:17:49 +0200

    added camera zoom

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,10 +1,9 @@
-
 import { useEffect, useRef } from 'react';
 import './App.scss';
-
 function App() {
 
   const videoRef = useRef<HTMLVideoElement>(null);
+  let cameraSettings: any = null;
 
   const openFullscreen = (htmlElement: any) => {
     if (htmlElement.requestFullscreen) htmlElement.requestFullscreen();
@@ -27,20 +26,143 @@ function App() {
             frameRate: { ideal: 60 }
           },
           audio: false
-        });;
+        });
+
+        if (videoRef.current?.srcObject) {
+          const track = (videoRef.current.srcObject as MediaStream).getVideoTracks()[0];
+          const capabilities: any = track.getCapabilities();
+          if (track && capabilities && 'zoom' in capabilities) {
+            const settings: any = track.getSettings();
+            cameraSettings = {
+              currentLevel: settings.zooom,
+              min: capabilities.zoom.min,
+              max: capabilities.zoom.max,
+              step: capabilities.zoom.step
+            }
+          } else { console.warn('Zoom not supported on this device'); }
+        }
       }
-    } catch (error) {
-      console.error("Error accessing selfie camera:", error);
+    } catch (error) { console.error("Error accessing selfie camera:", error); }
+  }
+  const setZoomLevel = async (zoomLevel: number = 1) => {
+    if (videoRef.current?.srcObject) {
+
+      const track = (videoRef.current.srcObject as MediaStream).getVideoTracks()[0];
+      const capabilities: any = track.getCapabilities();
+
+      if (track && capabilities && 'zoom' in capabilities) {
+        const settings: any = track.getSettings();
+        console.log('Current zoom:', settings.zoom);
+        console.log('Zoom range:', capabilities.zoom);
+
+        // Set zoom level (within supported range)
+        await track.applyConstraints({
+          advanced: [{ zoom: Math.min(zoomLevel, cameraSettings.max) }] as any // Example: zoom to 2x
+        });
+      } else {
+        console.warn('Zoom not supported on this device');
+      }
+    }
+  }
+  const getZoomObject = (): any => {
+    if (videoRef.current?.srcObject) {
+      const track = (videoRef.current.srcObject as MediaStream).getVideoTracks()[0];
+      const capabilities: any = track.getCapabilities();
+
+      if (track && capabilities && 'zoom' in capabilities) {
+        const settings: any = track.getSettings();
+        return settings.zooom;
+      } else {
+        console.warn('Zoom not supported on this device');
+        return null;
+      }
+    } else {
+      return null;
     }
   }
 
   useEffect(() => {
 
-    const bodyElement = document.getElementsByTagName('body')[0];
+    window.onresize = () => { /* console.log("RESIZING IS HAPPENING"); */ }
+    window.onwheel = () => { /* console.log("WHEEL IS ROLLING"); */ }
 
-    videoRef.current?.addEventListener('click', (event: Event) => {
-      document.fullscreenElement === bodyElement ? closeFullscreen(bodyElement) : openFullscreen(bodyElement)
-    });
+    /*const bodyElement = document.getElementsByTagName('body')[0];
+    (videoRef.current as HTMLElement).onclick = (event: Event) => {
+      // document.fullscreenElement === bodyElement ? closeFullscreen(bodyElement) : openFullscreen(bodyElement)
+    }*/
+
+    let onylOnce = true;
+    let startPoint: any = null;
+
+    // Horizontal -> 0; Vertical -> 1;
+    let initialDirection: number = 1;
+    let previousDistance: number = 0;
+    let diagonalDistance: number = 0;
+
+    if (window.PointerEvent && ('ontouchstart' in window || navigator.maxTouchPoints > 0 || window.matchMedia('(pointer: coarse)').matches)) {
+
+      (videoRef.current as HTMLElement).onpointerdown = (event: PointerEvent) => {
+        onylOnce = true;
+        initialDirection = 1;
+        diagonalDistance = previousDistance = 0;
+        startPoint = { x: event.clientX, y: event.clientY, time: Date.now() };
+      }
+
+      (videoRef.current as HTMLElement).onpointermove = (event: PointerEvent) => {
+
+        const getDirection = (distanceA: number, distanceB: number): number => {
+          // Value 0 represents HORIZONTAL direction, value 1 VERTICAL direction
+          return distanceA > distanceB ? 0 : 1;
+        }
+        const isZooming = (distance: number = 0, newDirection: number = 1) => {
+          const step = cameraSettings?.step ? cameraSettings.step : 0.01;
+          let zoomLevel: any = null;
+
+          if (initialDirection === newDirection) {
+            if (distance < previousDistance) {
+              //console.log("SMANJUJE SE");
+              zoomLevel = getZoomObject()?.currentLevel - step;
+            } else {
+              //console.log('POVEĆAVA SE');
+              zoomLevel = getZoomObject()?.currentLevel + step;
+            }
+            previousDistance = distance;
+            if (Number(zoomLevel)) {
+              setZoomLevel(zoomLevel)
+            }
+          } else {
+            initialDirection = newDirection;
+            startPoint = endPoint;
+            previousDistance = 0;
+          }
+        }
+
+        let endPoint = { x: event.clientX, y: event.clientY, time: Date.now() };
+
+        let chatetusHorizontal = Math.abs(endPoint?.x - startPoint?.x);
+        let chatetusVertical = Math.abs(endPoint?.y - startPoint?.y);
+        diagonalDistance = Math.pow(Math.pow(chatetusHorizontal, 2) + Math.pow(chatetusVertical, 2), 0.5);
+
+        if (onylOnce && diagonalDistance > 0) {
+          initialDirection = getDirection(Math.pow(chatetusHorizontal, 0.7), Math.pow(chatetusVertical, 0.7));
+          onylOnce = false;
+        }
+
+        if (onylOnce === false) {
+          if (initialDirection) {
+            isZooming((startPoint.y - endPoint.y), getDirection(Math.pow(chatetusHorizontal, 0.7), Math.pow(chatetusVertical, 0.7)));
+          } else {
+            isZooming((endPoint.x - startPoint.x), getDirection(Math.pow(chatetusHorizontal, 0.7), Math.pow(chatetusVertical, 0.7)));
+          }
+        }
+      }
+      (videoRef.current as HTMLElement).onpointerup = (event: PointerEvent) => {
+        //console.log("Razlika je: " + hypotenuse);
+      }
+      (videoRef.current as HTMLElement).onpointercancel = (event: PointerEvent) => {
+        //console.log("Razlika je: " + hypotenuse);
+      }
+    }
 
     navigator.permissions.query(({ name: "camera" } as any)).then(result => {
       console.log("Camera permission state:", result.state);
